9fb36db56c3a5712dd6e0f23ceab82daa9200879
iridescent-black <844030491@qq.com>
2023-09-13 21:58:26 +0800

    docs(*): 添加注释

--- src/leetcode/jian_zhi_offer/33.ts
+++ src/leetcode/jian_zhi_offer/33.ts
@@ -1,19 +1,30 @@
 /** 后序遍历
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
  * @see https://leetcode.cn/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/
  */
 export function verifyPostorder(postorder: number[]): boolean {
   return recur(postorder, 0, postorder.length - 1)
 }
 
-/** 判断 [i, j] 范围内的树是否满足二叉搜索树 */
-function recur(postorder: number[], i: number, j: number): boolean {
-  if (i >= j) return true
-  let tempIndex = i
-  while (postorder[tempIndex] < postorder[j]) tempIndex++
+/** 判断 [firstIndex, rootIndex] 范围内的树是否满足二叉搜索树
+ * @description 二叉搜索树定义：
+ * - 左子树 < 根节点 < 右子树
+ * - 左右子树都是二叉搜索树
+ * @description 后序遍历的结果是：[左子树 | 右子树 | 根节点]
+ * @param postorder 后序遍历结果
+ * @param firstIndex 左子树的第一个节点
+ * @param rootIndex 根节点
+ */
+function recur(postorder: number[], firstIndex: number, rootIndex: number): boolean {
+  // 判断是否越界
+  if (firstIndex >= rootIndex) return true
+  // 找到第一个大于根节点的值，即为右子树的根节点
+  let tempIndex = firstIndex
+  while (postorder[tempIndex] < postorder[rootIndex]) tempIndex++
   const firstRight = tempIndex
-  while (postorder[tempIndex] > postorder[j]) tempIndex++
-  // 如果 tempIndex !== j, 说明右子树有小于等于根节点的值，不满足
-  return (
-    tempIndex === j && recur(postorder, i, firstRight - 1) && recur(postorder, firstRight, j - 1)
-  )
+  // 判断右子树是否都大于根节点
+  while (postorder[tempIndex] > postorder[rootIndex]) tempIndex++
+  // 如果 tempIndex !== rootIndex, 说明右子树有小于等于根节点的值，不满足
+  if (tempIndex !== rootIndex) return false
+  return recur(postorder, firstIndex, firstRight - 1) && recur(postorder, firstRight, rootIndex - 1)
 }
