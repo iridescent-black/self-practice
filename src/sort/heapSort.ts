b4b5cc86b1fb8a0d0eec156e83becc48590904d4
iridescent-black <844030491@qq.com>
2023-10-09 01:13:33 +0800

    refactor(*): 优化堆排序可读性

--- src/sort/heapSort.ts
+++ src/sort/heapSort.ts
@@ -1,5 +1,9 @@
 import { swap } from '../utils'
 
+/** 堆排序
+ * - 时间复杂度 O(nlogn)：建堆 O(n) + 排序 O(nlogn)
+ * - 空间复杂度 O(1)
+ */
 export function heapSort(arr: number[]) {
   const length = arr.length
   if (length <= 1) return arr
@@ -11,34 +15,34 @@ export function heapSort(arr: number[]) {
 
   return arr
 
-  // 大顶堆 root item 是最大值，每次把 rootIndex 和未排序的区间最后一位交换位置
-  // 重新堆化
+  // 大顶堆 root item 是最大值，每次把 rootIndex 和未排序的区间最后一位交换位置，然后重新堆化
   function sort() {
-    let maxIndex = length - 1
-    while (maxIndex > 0) {
-      swap(arr, 0, maxIndex)
-      maxIndex -= 1
-      heapify(arr, maxIndex + 1, 0)
+    let lastIndex = length - 1
+    while (lastIndex > 0) {
+      swap(arr, 0, lastIndex)
+      lastIndex -= 1
+      heapify(0, lastIndex + 1)
     }
   }
 
   function buildHeap() {
+    // 完全二叉树，(length - 1) / 2 后面的节点都是叶子节点，不需要堆化
     for (let i = Math.floor((length - 1) / 2); i >= 0; i--) {
-      heapify(arr, length, i)
+      heapify(i, length)
     }
   }
 
-  function heapify(arr: number[], length: number, rootIndex: number) {
-    let i = rootIndex
+  function heapify(rootIndex: number, length: number) {
+    let currRootIndex = rootIndex
     while (true) {
-      let maxIndex = i
-      const leftIndex = i * 2 + 1
-      const rightIndex = i * 2 + 2
-      if (leftIndex < length && arr[leftIndex]! > arr[maxIndex]!) maxIndex = leftIndex
-      if (rightIndex < length && arr[rightIndex]! > arr[maxIndex]!) maxIndex = rightIndex
-      if (maxIndex === i) break
-      swap(arr, i, maxIndex)
-      i = maxIndex
+      let maxIndex = currRootIndex
+      const leftIndex = currRootIndex * 2 + 1
+      const rightIndex = currRootIndex * 2 + 2
+      if (leftIndex < length && arr[leftIndex] > arr[maxIndex]) maxIndex = leftIndex
+      if (rightIndex < length && arr[rightIndex] > arr[maxIndex]) maxIndex = rightIndex
+      if (maxIndex === currRootIndex) break
+      swap(arr, currRootIndex, maxIndex)
+      currRootIndex = maxIndex
     }
   }
 }
